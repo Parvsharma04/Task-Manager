caf41336ca75589fc6829a95960be6bf190699b5
Parv Sharma <sharmaparv.2004@gmail.com>
2024-03-10 13:01:20 +0530

    validations added

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,8 +4,9 @@ const getAllTasks = (req, res)=>{
     res.send('Get All Tasks')
 }
 
-const createTask = (req, res)=>{
-    res.json(req.body)
+const createTask = async (req, res)=>{
+    const task = await Task.create(req.body)
+    res.status(201).json({task})
 }
 const getTask = (req, res)=>{
     res.json({id : req.params.id})
